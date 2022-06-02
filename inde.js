d5f61d72329f3873ed667533c19fce8ec8d13609
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-06-02 16:02:49 +0500

    hello

---
+++ inde.js
@@ -0,0 +1,124 @@
+// Data type
+// primitiv data type
+// number, string, boolean, undefined, null
+
+// noprimitiv data type
+// object -> (array, function)
+
+/////// callstack //////// heap
+
+// number -> son 2
+// string -> text '' "" ``
+// boolean -> true false
+// undefined -> hech narsa yoq bolsa
+// null -> hech qaysi data typega otmagan bolsa
+// object -> {name:'', surname:''}
+
+// console.log() -> terminalga malumot chiqarish uchun
+
+// primativ   1 string 2 number 3 boolean 4 undifaind 5 null 
+//non prmativ   objeckt arrey reg-ex
+
+// let one =123
+//  one=one2 ='hello'
+// console.log( one2);
+
+// const one ='hello world'
+
+// console.log(one);
+
+// var one =1.2222
+// console.log(Number.parseInt("4.666"));
+// console.log(Number.isInteger(6767));
+// console.log(one.toFixed(3));
+
+// // 2 yuzasini topish 
+// let s ;
+// let a =4
+// s=a**2
+// console.log(s);
+
+// 3 yuzasi va parametri 
+// var s ;
+// var a=5, b=10
+// s=a*b
+// console.log(s);
+// var p ;
+// p=2*(a+b)
+// console.log(p);
+
+// 4 masala 
+// var p =3.14
+// var d = 10
+// l=p*d 
+// console.log(l);
+
+// // 5 masala 
+// var v;
+// var s;
+// var a=10
+// v=a**3
+// s=6*a**2
+// console.log(s);
+// console.log(v);
+
+//  6 masla 
+// var a=10 
+// var b=10
+// var c=10
+// var v=a*b*c 
+// var s=2*(a*b+b*c+a*c)
+// console.log(v);
+// console.log(s);
+// console.log(c); 
+
+// 7masala 
+
+// const pi =3.14
+// var r= 5
+// l=2*pi*r
+// s=pi*r**2
+// console.log(s);
+// console.log(l);
+
+
+// 8 masala 
+// var a=10 
+// var b=10
+// s = (a+b)/2
+// console.log(s);
+
+// 9 masala  
+// var  a=10
+// var b=10
+// var s=a*b/2
+// console.log(s);
+
+// 10 masala 
+// var a=5
+// var b=5
+// s=a+b 
+// d=a*b
+// c=a**b
+// console.log(s);
+// console.log(d); 
+// console.log(c); 
+
+
+// 11 masala 
+// var a=5
+// var b=5
+// var d=a+b
+// var s=a*b
+// var x=a-b
+// console.log(d);
+// console.log(s);
+// console.log(x);
+
+// 12 masala 
+// a=10
+// b=10
+// c = a**2+b**2 
+// p=a+b+c
+// console.log(c);
+// console.log(p);
