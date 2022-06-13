ddbb9228bee13c12853d8ab272df885e998d0b2b
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-06-14 00:25:56 +0500

    done

--- one.js
+++ one.js
@@ -434,3 +434,531 @@
 // const one=13244
 
 // console.log(one);
+
+
+
+// 7 kyu
+// V A P O R C O D E
+// JavaScript:
+// function vaporcode(string) {
+//     return string.toUpperCase().split('').filter(v=> v !== ' ').join('  ')
+//   } 
+// 28 days agoRefactorDiscuss
+// 8 kyu
+// Reversed Strings
+// JavaScript:
+// function solution(str){
+//    b= str.split('')
+// return b.reverse().join('')   
+
+// }
+// last monthRefactor
+// function solution(str){
+//  b= str.split('')
+// return b.reverse().join('')
+// }
+// last monthRefactorDiscuss
+
+// function solution(str){
+//     var one = str.split('').reverse().join('')
+//    return one
+//     }
+// last monthRefactorDiscuss
+// 7 kyu
+// Find the capitals
+// JavaScript:
+//    var capitals = function (word) {
+//         var b = []
+//   for (let i = 0; i < word.length; i++) {
+//       if (word[i]==word[i].toUpperCase()) {
+//           b.push(i)
+//       }
+      
+//     }
+//     return b
+//     }
+// last monthRefactorDiscuss
+// 8 kyu
+// Double Char
+// JavaScript:
+// function doubleChar(str){
+//         var one  = ''
+//         for (let i = 0; i < str.length; i++) {
+           
+//                one+=str[i]+str[i]+""
+//            }
+            
+        
+      
+//           return one
+      
+      
+//           }
+// last monthRefactorDiscuss
+// 7 kyu
+// Sort Numbers
+// JavaScript:
+// function solution(nums){
+    
+// if(nums==null) {
+//     return []
+
+// }
+// return nums.sort((a,b)=> a-b)
+//    }
+// last monthRefactor
+// const solution = nums => nums == null ? [] : nums.sort((a, b) => a - b)
+// 2 months agoRefactorDiscuss
+// 8 kyu
+// Convert a String to a Number!
+// JavaScript:
+// var stringToNumber = function(str){
+
+//   return +str
+// }
+// last monthRefactorDiscuss
+// var stringToNumber = function(str){
+//   return +str;
+// }
+// 2 months agoRefactor
+// 7 kyu
+// Sort array by string length
+// JavaScript:
+// function sortByLength (array) {
+//    var b = array.sort((a,b)=>a.length-b.length)
+//     return b
+//     }
+// last monthRefactorDiscuss
+// 7 kyu
+// Shortest Word
+// JavaScript:
+// function findShort(s){
+//     var res=[]
+//     var a= s.split(' ')
+//     var b=a.map((i)=>{
+//         res.push(i.length)
+//     })
+//     return res.sort((a,b)=>a-b)[0]
+     
+//    }
+// last monthRefactorDiscuss
+// 7 kyu
+// Highest and Lowest
+// JavaScript:
+// function highAndLow(numbers){
+//      var num=numbers.split(" ")
+//      console.log(num);
+
+//     var res=[]
+//     for (let i = 0; i < num.length; i++) {
+//         res.push(+num[i])
+        
+//     }
+//     var l=res.sort((as,b)=>as-b)
+//     return `${l[res.length-1]} ${l[0]}`
+    
+    
+//     }
+// last monthRefactorDiscuss
+// 7 kyu
+// Remove the minimum
+// JavaScript:
+// function removeSmallest(numbers) {
+//     var a= Math.min(...numbers)
+//     var b=numbers.indexOf(a)
+
+//     var res=[]
+//     for (let i = 0; i < numbers.length; i++) {
+//         if(i!==b){
+//             res.push(numbers[i])
+//         }
+        
+        
+//     }
+//     return res
+
+// }
+// last monthRefactor
+// 8 kyu
+// Reversing Words in a String
+// JavaScript:
+
+// function reverse(string){
+// b = string.split(' ').reverse().join(` `)
+// return b
+// }
+// last monthRefactorDiscuss
+// 8 kyu
+// Convert a string to an array
+// JavaScript:
+// function stringToArray(string){
+// return string.split(' ')
+
+// }
+// last monthRefactor
+// function stringToArray(string){
+// return string.split(' ')
+//   // code code code
+
+// }
+// 2 months agoRefactorDiscuss
+// 8 kyu
+// Remove exclamation marks
+// JavaScript:
+// function removeExclamationMarks(s) {
+//   return s.replace(/!/g,'' )
+// }
+// last monthRefactorDiscuss
+// 8 kyu
+// Array plus array
+// JavaScript:
+// function arrayPlusArray(arr1, arr2) {
+//   b= arr1.concat(arr2) 
+//   return  b.reduce((a,b)=> a+b)
+//    }
+// last monthRefactorDiscuss
+// 8 kyu
+// Sentence Smash
+// JavaScript:
+// function smash (words) {
+//    return words.join(' ')
+// };
+// last monthRefactorDiscuss
+// 4 kyu
+// Sum Strings as Numbers
+// JavaScript:
+// function sumStrings(a,b) { 
+//   if ( b.length > a.length ){
+//     var temp = a ;
+//     a = b ;
+//     b = temp ;
+//   }
+//   while ( b.length < a.length ){
+//     b = '0' + b ;
+//   }
+//   a = a.split('').reverse().join('');
+//   b = b.split('').reverse().join('');
+//   var c = '' , i = 0 ;
+//   var carry = 0 ;
+//   for ( var i = 0 ; i < a.length ; i ++ ) {
+//     var result = parseInt(a[i]) + parseInt(b[i]) + carry ;
+//     carry = 0 ;
+//     if ( result >= 10 ){
+//       result -= 10 ;
+//       carry ++ ;
+//     }
+//     c = c + result;
+//   }
+//   if ( carry !== 0 ){
+//     c = c + '1' ;
+//   }
+//   while ( c[c.length-1] === '0' ){
+//     c = c.slice(0,c.length-1) ;
+//   }
+//   c = c.split('').reverse().join('') ;
+//   return c ;
+// }
+// last monthRefactorDiscuss
+// 8 kyu
+// Jenny's secret message
+// JavaScript:
+// function greet(name){
+//   var one = name.padStart(9,'hello ')
+//     one=    name.padStart(10,'hello ')
+//       one = name.padStart(12,'hello ')
+//      one = "Hello, " + name + "!";
+//      if(name === "Johnny")
+//        one = "Hello, my love!";
+//   return one
+//     }
+// last monthRefactorDiscuss
+// 8 kyu
+// Count of positives / sum of negatives
+// JavaScript:
+// function countPositivesSumNegatives(input) {
+//   if (!input || input.length === 0) return []
+
+//   const positives = input
+//     .filter(number => Math.sign(number) === 1)
+//     .reduce(a => a + 1, 0)
+
+//   const negatives = input
+//     .filter(number => Math.sign(number) === -1)
+//     .reduce((a, b) => a + b, 0)
+
+//   return [positives, negatives]
+// }
+// last monthRefactorDiscuss
+// 8 kyu
+// Remove String Spaces
+// JavaScript:
+// function noSpace(x){
+//  return x.split(' ').join('')
+// }
+// last monthRefactorDiscuss
+// 8 kyu
+// Reversed Words
+// JavaScript:
+// function reverseWords(str){
+//      return str.split(' ').reverse( ).join(` `)
+//   }
+// last monthRefactorDiscuss
+// 8 kyu
+// Grasshopper - Summation
+// JavaScript:
+
+// var summation = function (num) {
+
+// var str =0
+//     for (let i = 0; i <= num; i++) {
+// str+=i
+        
+// }
+// return str
+
+// }
+// last monthRefactor
+// 7 kyu
+// Isograms
+// JavaScript:
+// function isIsogram(str){
+//   var one  = ''
+//   for (let i = 0; i < str.length; i++) {
+//      if (!one.includes(str[i].toLowerCase())) {
+//          one+=str[i].toLowerCase()
+    
+//         }
+//   }
+// if (one=='' ) {
+//     return true
+// }
+
+//     return str.length==one.length
+
+
+//     }
+// last monthRefactorDiscuss
+// 8 kyu
+// Sum Mixed Array
+// JavaScript:
+// function sumMix(x){
+//   return x.reduce((x, y) => Number(x) + Number(y), 0);
+// }
+// last monthRefactorDiscuss
+// 8 kyu
+// Count by X
+// JavaScript:
+// function countBy(x, n) {
+
+// arr= []
+
+// for(i=x; i<=x*n; i+=x ){
+// arr.push(i)
+ 
+// }
+// return arr
+
+
+// }
+// last monthRefactor
+// 8 kyu
+// Is it even?
+// JavaScript:
+// function testEven(n) {
+//    return (n%2 == 0)? true : false
+// }
+// testEven(1)
+// last monthRefactorDiscuss
+// 8 kyu
+// Difference of Volumes of Cuboids
+// JavaScript:
+// function findDifference(a, b) {
+
+//   let getA = a.reduce((a,b)=> a*b)
+//   let getB = b.reduce((a,b)=> a*b)
+
+//   return Math.abs(getA - getB)
+
+// }
+// last monthRefactorDiscuss
+// 6 kyu
+// IP Validation
+// JavaScript:
+// function isValidIP(str) {
+//   var arr = str.split('.')
+//   arr=arr.filter((i)=> { 
+//     return  !! (i >= 0 && i <=255 && i==String(+i))
+// })
+//   return arr.length==4
+// }
+// last monthRefactorDiscuss
+// 6 kyu
+// Detect Pangram
+// JavaScript:
+// function isPangram(string){
+//   string = string.toLowerCase();
+//   return "abcdefghijklmnopqrstuvwxyz"
+//     .split("").every(function(x){
+//       return string.indexOf(x) !== -1;
+//   });
+// }
+// last monthRefactorDiscuss
+// 8 kyu
+// Cat years, Dog years
+// JavaScript:
+// var humanYearsCatYearsDogYears = function(humanYears) {
+
+//   if (humanYears === 1) {return [1, 15, 15];}
+//   if (humanYears === 2) {return [2, 24, 24];}
+  
+//   return [humanYears, 24+((humanYears-2)*4), 24+((humanYears-2)*5)];
+// }
+// 2 months agoRefactorDiscuss
+// 8 kyu
+// Opposite number
+// JavaScript:
+//  const opposite = n => n-n-n
+// 2 months agoRefactorDiscuss
+// 7 kyu
+// Vowel Count
+// JavaScript:
+// function getCount(str) {
+//   let strStr = 'aeuio'
+//   return str.split('').filter((v)=> strStr.includes(v)).length;
+// }
+// 2 months agoRefactorDiscuss
+// 6 kyu
+// Assemble string
+// JavaScript:
+// function assembleString(arr){
+//   let r = []
+//   for(let i = 0; i<arr.length; i++){
+//     for(let j = 0; j<arr[i].length; j++){
+//       if(i==0) r.push(arr[i][j])
+//       if(r[j] == '*' || arr[i][j] !== '*') r[j] = arr[i][j]
+//     }
+//   }
+//   return r.map(x=>x=='*' ? '#' : x).join('')
+// }
+// 2 months agoRefactorDiscuss
+// 6 kyu
+// Who likes it?
+// JavaScript:
+// function likes(names) {
+//     if (names.length == 0) return `no one likes this`
+//     if (names.length == 1) return  `${names[0]} likes this`
+//     if (names.length == 2) return `${names[0]} and ${names[1]} like this`
+//     if (names.length == 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`
+//     if (names.length >= 4) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+// };
+// 2 months agoRefactorDiscuss
+// 8 kyu
+// String repeat
+// JavaScript:
+// function repeatStr (n, s) {
+//     return s.repeat(n)
+// }
+// 2 months agoRefactorDiscuss
+// 8 kyu
+// Remove First and Last Character
+// JavaScript:
+// function removeChar(str){
+// return str.slice(1,-1)
+
+// };
+
+
+
+// 2 months agoRefactorDiscuss
+// 8 kyu
+// Return Negative
+// JavaScript:
+// function makeNegative(num) {
+//   if(num === 0) {
+//     return 0
+//   }
+//   return -Math.abs(num)
+// } 
+// 2 months agoRefactorDiscuss
+// 5 kyu
+// Simple Pig Latin
+// JavaScript:
+// function pigIt(str){
+//  return str.replace(/(\w)(\w*)(\s|$)/g, "\$2\$1ay\$3")
+// }
+// 2 months agoRefactorDiscuss
+// 8 kyu
+// Basic Mathematical Operations
+// JavaScript:
+// function basicOp(operation, value1, value2)
+// {
+//   return eval(`${value1} ${operation} ${value2}`)
+// }
+// 2 months agoRefactor
+// function basicOp(operation, value1, value2){
+//   if(operation == '+') return value1 + value2
+//   if(operation == '-') return value1 - value2
+//   if(operation == '*') return value1 * value2
+//   if(operation == '/') return value1 / value2
+// }
+// 2 months agoRefactorDiscuss
+// 8 kyu
+// Sum of positive
+// JavaScript:
+// function positiveSum(arr) {
+//   let result = 0;
+//   arr = arr.filter((v) => v > 0);
+//   arr.length > 0 ? arr.map((v) => (result += v)) : result = 0
+//   return result;
+// }
+// 2 months agoRefactorDiscuss
+// 8 kyu
+// String cleaning
+// JavaScript:
+// function stringClean(s){
+//   return s.replace(/[0-9]/g, "");
+// }
+// 2 months agoRefactor
+// 8 kyu
+// Convert number to reversed array of digits
+// JavaScript:
+// function digitize(n) {
+//   var b = n
+//     .toString()
+//     .split("")
+//     .map((v) => +v)
+//     .reverse();
+
+//   return b;
+// }
+// 2 months agoRefactorDiscuss
+// 8 kyu
+// Convert boolean values to strings 'Yes' or 'No'.
+// JavaScript:
+// function boolToWord( bool ){
+//   return bool ?   'Yes':'No'
+// }
+// 2 months agoRefactorDiscuss
+// 6 kyu
+// Counting Duplicates
+// JavaScript:
+// function duplicateCount(text){
+
+//   return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+// }
+// 2 months agoRefactorDiscuss
+// 8 kyu
+// Simple multiplication
+// JavaScript:
+// function simpleMultiplication(number) {
+// return number % 2 == 0 ? number * 8 : number * 9;
+// }
+// 2 months agoRefactor
+// 6 kyu
+// Your order, please
+// JavaScript:
+// function order(words){
+//  return words.split(' ').sort(function (a, b) {
+//         return a.match(/\d/) - b.match(/\d/);
+//     }).join(' ');
+// }
