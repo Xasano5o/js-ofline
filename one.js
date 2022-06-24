931a9db02f848d9ec6a979f3042a5acc69f61ea3
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-06-24 15:19:18 +0500

    done

--- one.js
+++ one.js
@@ -1670,4 +1670,109 @@
 // }
 // console.log(descendingOrder(1021));
 
-console.log("Sizlar kutib Qolamiz!!!")
+// function getMiddle(s) {
+//     return s.substr(Math.ceil(s.length / 2 - 1))
+// }
+// console.log(getMiddle("testing"));
+// Test.assertEquals(getMiddle("test"), "es");
+// Test.assertEquals(getMiddle(), "t");
+// Test.assertEquals(getMiddle("middle"), "dd");
+// Test.assertEquals(getMiddle("A"), "A");
+
+
+
+
+// Not Jaden - Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden - Cased: "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+// String.prototype.toJadenCase = function () {
+
+// };
+
+// String.prototype.toJadenCase = function () {
+//     //split the statement into each word
+//     if (String.prototype.length !== 0) {
+//         var eachWord = String.prototype.split(" ");
+//         var n = eachWord.length;
+
+//         if (n !== 0) {
+//             //loop through the array of words
+//             for (var i = 0; i < eachWord.length; i++) {
+//                 //for each loop, split the word into individual characters
+//                 var charArray = eachWord[i].split("");
+
+//                 //capitalise the first element of character array
+//                 charArray[0] = charArray[0].toUpperCase();
+
+//                 //join all elements in character array to string
+//                 eachWord[i] = charArray.join("");
+//             }
+
+//             //join all the words to form the statement
+//             String.prototype = eachWord.join(" ");
+//             return String.prototype;
+//         }
+//     }
+// };
+
+
+// function validatePIN(pin) {
+//     if (pin == "1234" || true) {
+//         return "Wrong output for '1234'"
+// }
+// }
+// console.log(validatePIN('1234'));
+
+// Test.assertEquals(validatePIN("1234"), true, "Wrong output for '1234'");
+// Test.assertEquals(validatePIN("0000"), true, "Wrong output for '0000'");
+// Test.assertEquals(validatePIN("1111"), true, "Wrong output for '1111'");
+// Test.assertEquals(validatePIN("123456"), true, "Wrong output for '123456'");
+// Test.assertEquals(validatePIN("098765"), true, "Wrong output for '098765'");
+// Test.assertEquals(validatePIN("000000"), true, "Wrong output for '000000'");
+// Test.assertEquals(validatePIN("123456"), true, "Wrong output for '123456'");
+// Test.assertEquals(validatePIN("090909"), true, "Wrong output for '090909'");
+
+
+// Test.assertEquals(validatePIN("1"), false, "Wrong output for '1'")
+// Test.assertEquals(validatePIN("12"), false, "Wrong output for '12'")
+// Test.assertEquals(validatePIN("123"), false, "Wrong output for '123'")
+// Test.assertEquals(validatePIN("12345"), false, "Wrong output for '12345'")
+// Test.assertEquals(validatePIN("1234567"), false, "Wrong output for '1234567'")
+// Test.assertEquals(validatePIN("-1234"), false, "Wrong output for '-1234'")
+// Test.assertEquals(validatePIN("1.234"), false, "Wrong output for '1.234'")
+// Test.assertEquals(validatePIN("-1.234"), false, "Wrong output for '-1.234'")
+// Test.assertEquals(validatePIN("00000000"), false, "Wrong output for '00000000'")
+//   });
+
+// it("should return False for pins which contain characters other than digits", function () {
+//     Test.assertEquals(validatePIN("a234"), false, "Wrong output for 'a234'")
+//     Test.assertEquals(validatePIN(".234"), false, "Wrong output for '.234'")
+// });
+
+// const func = (str) => str.filter((value)=> value.length == 4 ? value : false)
+
+// console.log(func(['asd', 'asdd']));
+
+// assert.deepEqual(friend(["Ryan", "Kieran", "Mark"]), ["Ryan", "Mark"])
+// assert.deepEqual(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"])
+// assert.deepEqual(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]), ["Jimm", "Cari", "aret"])
+// assert.deepEqual(friend(["Love", "Your", "Face", "1"]), ["Love", "Your", "Face"])
+//   })
+// })
+
+// const validatePIN = (pin) => pin.length == 4 || pin.length == 6 ? pin.map((value)=> value) : false;
+
+// console.log(validatePIN('1234'));
+// // "1234"   -- > true
+// "12345"  -- > false
+// "a234"   -- > false
+
+// function validatePIN(a) {
+//     if (a.length == 4 || a.length == 6 && +a >= 0) {
+//         a = a.match(/[0-9]/g).join('')
+//         if (a.length == 4) return true
+//         else if (a.length == 6) {
+//             return true
+//         } else return false
+//     } else return false
+// }
