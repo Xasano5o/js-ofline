487c40d64e65aaa9b56ae22ff218d51dbc271155
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-07-01 15:54:31 +0500

    done

--- one.js
+++ one.js
@@ -2292,24 +2292,24 @@ const data = [
 // function _clear_() {
 //     clearTimeout(timerId);
 // } 
-clearTimeout(timerId)
-function isOdd(number) {
-    return number % 2 != 0;
-}
-function isEven(number) {
-    return number % 2 == 0;
-} 
+// clearTimeout(timerId)
+// function isOdd(number) {
+//     return number % 2 != 0;
+// }
+// function isEven(number) {
+//     return number % 2 == 0;
+// } 
 
-function filter(numbers, fn) {
-    let results = [];
-    for (const number of numbers) {
-        if (fn(number)) {
-            results.push(number);
-        }
-    }
-    return results;
-}
-let numbers = [1, 2, 4, 7, 3, 5, 6];
+// function filter(numbers, fn) {
+//     let results = [];
+//     for (const number of numbers) {
+//         if (fn(number)) {
+//             results.push(number);
+//         }
+//     }
+//     return results;
+// }
+// let numbers = [1, 2, 4, 7, 3, 5, 6];
 
-console.log(filter(numbers, isOdd));
-console.log(filter(numbers, isEven));
+// console.log(filter(numbers, isOdd));
+// console.log(filter(numbers, isEven));
