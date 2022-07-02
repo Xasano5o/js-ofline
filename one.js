75206835adc28ee54b9f34d7a00f821a1e6cea60
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-07-02 12:47:49 +0500

    done

--- one.js
+++ one.js
@@ -2376,3 +2376,35 @@ const data = [
 // console.log(b);
 // console.log(b);
 // console.log(a);
+
+// var obj = {
+//     name: "vivek",
+//     getName: function () {
+//         console.log(this.name);
+//     }
+
+// }
+
+// var getName = obj.getName;
+
+// var obj2 = { name: "akshay", getName };
+// obj2.getName();
+
+// var obj1 = {
+//     address: "Mumbai,India",
+//     getAddress: function () {
+//         console.log(this.address);
+//     }
+// }
+
+// var getAddress = obj1.getAddress;
+// var obj2 = { name: "akshay" };
+// console.log(obj2.getAddress());
+
+// function sayHello() {
+//     return "Hello " + this.name;
+// }
+
+// var obj = { name: "Sandy" };
+
+// console.log(sayHello.call(obj));
