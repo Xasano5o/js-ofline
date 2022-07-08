3612d2d1d67ee5bbef3c8b9df71ba09e88b9a2c5
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-07-08 21:14:45 +0500

    done

--- one.js
+++ one.js
@@ -2934,5 +2934,5 @@
 //    var a = b = 3;
 // })();
 
-console.log("a defined? " + (typeof a !== 'undefined'));
-console.log("b defined? " + (typeof b !== 'undefined'));
+// console.log("a defined? " + (typeof a !== 'undefined'));
+// console.log("b defined? " + (typeof b !== 'undefined'));
