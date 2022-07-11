855c9b424ba0d75df7b0f25eff2905cc280dc6bb
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-07-11 16:48:39 +0500

    done

--- one.js
+++ one.js
@@ -1929,26 +1929,26 @@
 
 
 // doom orqali ulangan 
-// const data = [
-//     { id: 1, status: 're-exam', name: 'Rakhimjonov Davronbek' },
-//     { id: 2, status: 'passed', name: 'Komil Muhammadiyev' },
-//     { id: 3, status: 'passed', name: 'Mahkamov Tohirbek' },
-//     { id: 4, status: 'failed', name: 'Nuraliev Zuhriddin' },
-//     { id: 5, status: 're-exam', name: 'Dilshodbek Boymamatov' },
-//     { id: 6, status: 'passed', name: 'Muhharam Ozodboyeva' },
-//     { id: 7, status: 'failed', name: 'Azimov Umarbek' },
-//     { id: 8, status: 'passed', name: 'Abdurazzaqov Xasan' },
-//     { id: 9, status: 'passed', name: 'Davlyatov MuhammadAli' },
-//     { id: 10, status: 'passed', name: 'Zohidova Ruqiya' },
-//     { id: 11, status: 'passed', name: 'Abduhamidov Nurbek' },
-//     { id: 12, status: 'passed', name: 'Nizamov Sardorbek' },
-// ]; 
+const data = [
+    { id: 1, status: 're-exam', name: 'Rakhimjonov Davronbek' },
+    { id: 2, status: 'passed', name: 'Komil Muhammadiyev' },
+    { id: 3, status: 'passed', name: 'Mahkamov Tohirbek' },
+    { id: 4, status: 'failed', name: 'Nuraliev Zuhriddin' },
+    { id: 5, status: 're-exam', name: 'Dilshodbek Boymamatov' },
+    { id: 6, status: 'passed', name: 'Muhharam Ozodboyeva' },
+    { id: 7, status: 'failed', name: 'Azimov Umarbek' },
+    { id: 8, status: 'passed', name: 'Abdurazzaqov Xasan' },
+    { id: 9, status: 'passed', name: 'Davlyatov MuhammadAli' },
+    { id: 10, status: 'passed', name: 'Zohidova Ruqiya' },
+    { id: 11, status: 'passed', name: 'Abduhamidov Nurbek' },
+    { id: 12, status: 'passed', name: 'Nizamov Sardorbek' },
+]; 
 
 
-// function update(id,key,val) {
-//     return data.map((value)=>value.id===id ? {...value,[key]:val}:value)
-//     // return data.map((value)=>value.id===id ?{...value, [key]:val}:value )
-// }
+function update(id,key,val) {
+    return data.map((value)=>value.id===id ? {...value,[key]:val}:value)
+    // return data.map((value)=>value.id===id ?{...value, [key]:val}:value )
+}
 // console.log(update(11,'name','xolnmat'));
 
 // function get(text) {
